1c2e3c4d178927330ba28bd80084e011e22485a6
DannyBoyNYC <daniel.deverell@gmail.com>
2023-06-08 21:31:06 -0400

    done

--- app/js/index.js
+++ app/js/index.js
@@ -7,3 +7,35 @@ fetch(API)
   .then(function (data) {
     document.querySelector(".quote").innerText = data[0];
   });
+
+//  ====
+
+const el = document.querySelector("h2");
+
+const basilChefs = ["mama", "papa", "baby"];
+const basilTexture = ["greasy", "frozen", "spicy"];
+
+function random(array) {
+  const max = array.length;
+  const randomIndex = Math.floor(Math.random() * max);
+  return array[randomIndex];
+}
+
+var recipeName = `${random(basilChefs)}'s ${random(basilTexture)} pesto`;
+
+el.innerHTML = recipeName;
+
+// ===
+
+var modal = document.querySelector(".modal");
+// var betaButton = document.querySelector('.beta');
+
+function showPopover(event) {
+  console.log(event.target);
+  if (!event.target.matches(".beta")) return;
+  modal.classList.toggle("open");
+  event.preventDefault();
+}
+
+// betaButton.addEventListener('click', showPopover);
+document.addEventListener("click", showPopover);
