ef7efe5c139eea6e6abe5593a750e744d2cfa83c
DannyBoyNYC <daniel.deverell@gmail.com>
2023-06-08 20:41:25 -0400

    ready for JS

---
+++ app/js/index.js
@@ -0,0 +1,9 @@
+var API = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+fetch(API)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    document.querySelector(".quote").innerText = data[0];
+  });
